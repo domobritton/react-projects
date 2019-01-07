751aecb5cfdcadde48ebb78f01899e7894a8d420
domobritton <domonicbritton@gmail.com>
2019-01-07 13:53:05 -0800

    Reusable portal component

--- src/App.js
+++ src/App.js
@@ -2,18 +2,22 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Toggle from './ToggleRPC';
+import Portal from './Portal';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="">
+        <header className="header">
         </header>
         <Toggle>
           {({on, toggle}) => (
           <>
             {on && <h1>Show Me</h1>}
             <button onClick={toggle}>Show / Hide</button>
+            <Portal>
+              {on && <h1>Hi, I'm in a portal!</h1>}
+            </Portal>
           </>
           )}
         </Toggle>
