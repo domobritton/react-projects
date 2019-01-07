ebd02e3f2b79addfdc0216025cc7e2841e532e68
domobritton <domonicbritton@gmail.com>
2019-01-07 13:39:14 -0800

    Toggle button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Toggle from './ToggleRPC';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header className="">
         </header>
+        <Toggle>
+          {({on, toggle}) => (
+          <>
+            {on && <h1>Show Me</h1>}
+            <button onClick={toggle}>Show / Hide</button>
+          </>
+          )}
+        </Toggle>
       </div>
     );
   }
