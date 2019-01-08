9f31791d3f90476aaf740348a7e0128fa12be6e1
domobritton <domonicbritton@gmail.com>
2019-01-07 20:53:14 -0800

    Practice with react-spring and gesture

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import './App.css';
 import { Toggle } from 'Utilities';
 import User from './User';
 import UserProvider from './UserProvider';
-import { Card }  from 'Elements'
-import { Modal } from 'Elements'
+import { Card, Modal }  from 'Elements'
+import Drag from './Drag';
 class App extends Component {
   render() {
     return (
@@ -13,31 +13,11 @@ class App extends Component {
         <div className="App">
           <header className="header">
           </header>
-          <User />
+          <Drag />
           <Toggle>
             {({on, toggle}) => (
               <Fragment>
                 <button onClick={toggle}>Show / Hide</button>
-                {/* <Transition
-                  items={on}
-                  from={{ opacity: 0, bg: '#82d8d8', height: '0px'}}
-                  enter={{ opacity: 1, bg: '#524763', height: '200px'}}
-                  leave={{ opacity: 0, bg: '#82d8d8', height: '0px'}}
-                >
-                {on =>
-                  on && (props => 
-                  <Card style={{
-                    opacity: props.opacity,
-                    background: props.bg,
-                    overflow: 'hidden',
-                    height: props.height
-                  }}>
-                    <h1>Show Me</h1>
-                    <h3>{props.bg}</h3>
-                  </Card>
-                  )
-                }
-                </Transition> */}
                 <Modal on={on} toggle={toggle}>
                   <h1>Still in modal</h1>
                 </Modal>
